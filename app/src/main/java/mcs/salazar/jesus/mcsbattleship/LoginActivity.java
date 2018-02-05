300065bc009e1ac72799a91f7b836c9dd0e4539c
ivangb6 <ivangb6@gmail.com>
2018-02-05 16:36:58 -0500

    Implement Facebook Login Feature & add Butterknife: -  Add Facebook SDK -  Create LoginButton in activity_login -  Update Manifest with Facebook Activities -  Add Butterknife to Project

--- app/src/main/java/mcs/salazar/jesus/mcsbattleship/LoginActivity.java
+++ app/src/main/java/mcs/salazar/jesus/mcsbattleship/LoginActivity.java
@@ -1,13 +1,114 @@
 package mcs.salazar.jesus.mcsbattleship;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_facebook_button) LoginButton mFacebookButton;
+
+    private FirebaseAuth mAuth;
+
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        // Defined the information needed from user
+        mFacebookButton.setReadPermissions("email");
+        mFacebookButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("TRASH", "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("TRASH", "facebook:onCancel");
+                //TODO: Handle Facebook login cancellation
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("TRASH", "facebook:onError", error);
+                //TODO: Handle Facebook login error
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and act accordingly
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // If user is already signed-in
+        if (currentUser != null) {
+            //TODO: What to do if user is already signed-in
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("TRASH", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TRASH", "signInWithCredential:success");
+                            // Get user for future use
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TRASH", "signInWithCredential:failure",
+                                    task.getException());
+                            Toast.makeText(LoginActivity.this,
+                                    "Authentication failed! Please, try again",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
