de57c374ad95514bd02a573b3fc65ab04a92648a
bereket11 <bhaile@ucsc.edu>
2018-02-06 16:36:11 -0500

    Adding login using email and password

--- app/src/main/java/mcs/salazar/jesus/mcsbattleship/LoginActivity.java
+++ app/src/main/java/mcs/salazar/jesus/mcsbattleship/LoginActivity.java
@@ -23,8 +23,37 @@ import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.PluralsRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.TextKeyListener;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity   {
+
+    //defining views
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    // Firebase auth object
+    private FirebaseAuth firebaseAuth;
 
     public static final String TAG = LoginActivity.class.getName();
 
@@ -38,6 +67,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setupFacebookAuth();
+        setupEmailLogin();
+
+    }
+
+    private void setupFacebookAuth() {
         ButterKnife.bind(this);
 
         // Initialize Firebase Auth
@@ -66,7 +102,41 @@ public class LoginActivity extends AppCompatActivity {
                 //TODO: Handle Facebook login error
             }
         });
+    }
 
+    private void setupEmailLogin() {
+        // Gettting firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Log.i("Test", "TEST");
+        //initializing views
+        editTextEmail =  findViewById(R.id.editTextEmail);
+        editTextPassword =  findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.buttonSignin);
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editTextEmail.getText().toString();
+
+                String password = editTextPassword.getText().toString();
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Successful login", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast toast = Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG);
+                                    toast.show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 
     @Override
@@ -109,4 +179,6 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
+
+
 }
