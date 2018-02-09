d896db5a1ee20a24eac974f1a8e65b49d3f51ee4
ivangb6 <ivangb6@gmail.com>
2018-02-09 14:52:06 -0500

    Polish Coordinate and create method adjacentAvailableCoordinates() -  Create method in Util -  Change access type of Coordinate variables for easier access

--- app/src/main/java/mcs/salazar/jesus/mcsbattleship/Util.java
+++ app/src/main/java/mcs/salazar/jesus/mcsbattleship/Util.java
@@ -1,8 +1,47 @@
 package mcs.salazar.jesus.mcsbattleship;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mcs.salazar.jesus.mcsbattleship.models.Coordinate;
+
 /**
  * Created by Ivan on 2/8/2018
  */
 public class Util {
+
+    private static final int BATTLEFIELD_SIZE = 5;
+
+    /**
+     * Explore the tiles adjacent to the coordinate on the grid
+     * @param coordinate Chosen coordinate on the grid
+     * @param grid Grid representing battlefield
+     * @return List containing the available tiles around coordinate
+     */
+    public List<Coordinate> adjacentAvailableCoordinates(Coordinate coordinate, boolean[][] grid) {
+        List<Coordinate> availableTiles = new ArrayList<>();
+        // Check for the availability of adjacent cells of coordinate
+        if (!(coordinate.x - 1 < 0)) {
+            if (!grid[coordinate.x - 1][coordinate.y])
+                availableTiles.add(new Coordinate(coordinate.x - 1, coordinate.y));
+        }
+        if (!(coordinate.x + 1 >= BATTLEFIELD_SIZE)) {
+            if (!grid[coordinate.x + 1][coordinate.y])
+                availableTiles.add(new Coordinate(coordinate.x + 1, coordinate.y));
+        }
+        if (!(coordinate.y - 1 < 0)) {
+            if (!grid[coordinate.x][coordinate.y - 1])
+                availableTiles.add(new Coordinate(coordinate.x, coordinate.y - 1));
+        }
+        if (!(coordinate.y + 1 >= BATTLEFIELD_SIZE)) {
+            if (!grid[coordinate.x][coordinate.y + 1])
+                availableTiles.add(new Coordinate(coordinate.x, coordinate.y + 1));
+        }
+        Log.i("TRASH", "Available Tiles: " + availableTiles.size());
+        return availableTiles;
+    }
+
 
 }
--- app/src/main/java/mcs/salazar/jesus/mcsbattleship/models/Coordinate.java
+++ app/src/main/java/mcs/salazar/jesus/mcsbattleship/models/Coordinate.java
@@ -4,28 +4,12 @@ package mcs.salazar.jesus.mcsbattleship.models;
  * Created by Ivan on 2/8/2018
  */
 public class Coordinate implements Model {
-    private int mX;
-    private int mY;
+    public  int x;
+    public int y;
 
     public Coordinate(int x, int y) {
-        this.mX = x;
-        this.mY = y;
-    }
-
-    public int getX() {
-        return mX;
-    }
-
-    public void setX(int x) {
-        mX = x;
-    }
-
-    public int getY() {
-        return mY;
-    }
-
-    public void setY(int y) {
-        mY = y;
+        this.x = x;
+        this.y = y;
     }
 
     @Override
