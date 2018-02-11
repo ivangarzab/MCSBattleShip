8a86a399e4d2691c5ce479f7d2e06bf657167b73
ivangb6 <ivangb6@gmail.com>
2018-02-11 16:56:32 -0500

    Solve Matrix Parcelable issue and Polish Models

--- app/src/main/java/mcs/salazar/jesus/mcsbattleship/models/Model.java
+++ app/src/main/java/mcs/salazar/jesus/mcsbattleship/models/Model.java
@@ -1,9 +1,11 @@
 package mcs.salazar.jesus.mcsbattleship.models;
 
+import android.os.Parcelable;
+
 /**
  * Created by Ivan on 2/5/2018
  */
-public interface Model {
+public interface Model extends Parcelable {
     void toJson();
     void fromJson();
 }
