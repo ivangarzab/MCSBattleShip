1733cd082d6b4577fe7d47e88d46a53dd78d3a6e
ivangb6 <ivangb6@gmail.com>
2018-02-23 17:45:39 -0500

    Create SessionGameViewModel

---
+++ app/src/main/java/mcs/salazar/jesus/mcsbattleship/viewmodel/SessionGameViewModel.java
@@ -0,0 +1,15 @@
+package mcs.salazar.jesus.mcsbattleship.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+
+/**
+ * Created by Ivan on 2/23/2018
+ */
+public class SessionGameViewModel extends ViewModel implements MVVMViewModel {
+
+    @Override
+    public void bind() {
+
+    }
+}
