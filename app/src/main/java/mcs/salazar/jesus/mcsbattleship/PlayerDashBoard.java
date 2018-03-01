01f545e584d6000e38635b17a826ab88396816c8
bereket11 <bhaile@ucsc.edu>
2018-03-01 12:00:19 -0500

    add sign out

--- app/src/main/java/mcs/salazar/jesus/mcsbattleship/PlayerDashBoard.java
+++ app/src/main/java/mcs/salazar/jesus/mcsbattleship/PlayerDashBoard.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 /**
  * Created by berekethaile on 2/14/18.
@@ -14,17 +16,21 @@ import android.widget.Button;
 
 public class PlayerDashBoard extends AppCompatActivity {
 
-    private Button mPlay, mScores, mSessions, mFriends;
+    private Button mPlay, mScores, mSessions, mFriends, mSignout;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         mPlay = findViewById(R.id.name_game);
         mScores = findViewById(R.id.my_scores);
         mSessions = findViewById(R.id.sessions);
         mFriends = findViewById(R.id.friends);
+        mSignout = findViewById(R.id.sign_out);
 
         mPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +59,18 @@ public class PlayerDashBoard extends AppCompatActivity {
                 startActivity(new Intent(PlayerDashBoard.this, FriendsActivity.class));
             }
         });
+
+        mSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //signOut();
+                finish();
+            }
+        });
+    }
+
+    private void signOut() {
+        mFirebaseAuth.signOut();
     }
 
 }
